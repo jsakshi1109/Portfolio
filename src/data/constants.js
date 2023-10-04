1c2c739929fcc91f1b692d50e7c1ffded70e10eb
jsakshi1109 <129934661+jsakshi1109@users.noreply.github.com>
2023-10-04 23:08:38 +0530

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,257 @@
+export const Bio = {
+  name: "Sakshi Jain",
+  roles: [
+    "Android Developer",
+    "Front End Developer",
+    "Software Developer",
+    "Web Developer",
+    "Programmer",
+  ],
+  description:
+    "I'm Sakshi, a motivated individual aspiring to secure an entry-level position in technology, with a interest on front-end development. My true passion lies in crafting user-friendly experiences, and I'm thrilled at the opportunity to contribute to your company's objectives. Not only am I enthusiastic about applying my skills, but I'm also eager to expand my knowledge and embrace new technologies. Let's collaborate to innovate and create exceptional digital experiences! ",
+  github: "https://github.com/jsakshi1109",
+  resume:
+    "https://drive.google.com/file/d/18g7zWXmO8Vpqec2_Vl5myDX8knYRmdgU/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/sakshijain2003/",
+  twitter: "",
+  insta: "https://www.instagram.com/jsakshi1193/",
+  facebook: "",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "PHP",
+        image:
+          "https://www.php.net/images/logos/new-php-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/canva-logo.jpg?alt=media&token=dc208846-2873-4b56-b3d6-06cb1c239c1c",
+      },
+      {
+        name: "MS Office",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/MS-Office-logo.png?alt=media&token=996e6a02-2a81-46ff-b3ae-ccd8af516103",
+      },
+      {
+        name: "Adobe Photoshop",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/adobe-photoshop-logo.png?alt=media&token=c821e339-1c32-47a1-aec8-b0fc318f7a95",
+      },
+      {
+        name: "C",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/C_Logo.png?alt=media&token=2484612c-f227-4894-832e-3094fbd43d67",
+      },
+      {
+        name: "C++",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/c%2B%2Blogo.png?alt=media&token=34b80b09-83b1-43bb-b62f-f34b5634bdb8",
+      },
+      {
+        name: "C#",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/c%23-logo-icon-18.png?alt=media&token=7b81f7b1-65fd-4270-8801-e712faf76703",
+      },
+      {
+        name: ".NET",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/.net-logo.png?alt=media&token=dc83810f-606a-4d20-847d-3377c8674b6d",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "",
+    role: "Software Development Intern",
+    company: "MindAgile Consulting",
+    date: "May 2023 - July 2023",
+    desc: "I served as a Software Development Intern, crafting immersive designs in Figma and translating them into a robust UI using Node.js Express. I, along with my team, successfully deployed the application on Google App Engine, gaining hands-on experience in both design and development, contributing to a seamless user experience.",
+    skills: [
+      "Node JS",
+      "Spring boot",
+      "Google App Engine",
+      "Figma"
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/Completion%20Letter.jpg?alt=media&token=6377df88-eabc-462d-8c6a-674d2582b7a7",
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/Christ-Logo.jpg?alt=media&token=2b573858-eef6-453a-a275-eb7105990710",
+    school: "CHRIST(Deemed to be University), Bangalore",
+    date: "Aug 2021 - May 2024",
+    grade: "78%",
+    desc: "I am presently enrolled in a Bachelor's program in Computer Applications at CHRIST (Deemed to be University). Here, I am engaged in dynamic projects alongside a skilled team of developers. This experience not only enhances my learning but also provides valuable hands-on opportunities, allowing me to explore the exciting world of computer applications.",
+    degree: "BCA (Bachelor's in Computer Applications)",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/Sophia-Logo.jpg?alt=media&token=02d8ce09-c352-4e75-a86e-9ddb080e31f9",
+    school: "Sophia Girls' Sr. Sec. School, Bhilwara",
+    date: "May 2019 - July 2021",
+    grade: "95.6%",
+    desc: "I completed my Class 12 education at Sophia Girls' Sr. Sec. School, Bhilwara, majoring in Commerce with Computer Science. This program equipped me with essential skills and knowledge, laying the groundwork for my future academic and professional endeavors.",
+    degree: "CBSE(XII), Commerce with Computer",
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/CA-Logo.jpg?alt=media&token=01fc43f6-85d9-4ff8-9f74-9de2b593943f",
+    school: "Central Academy Sr. Sec. School, Bhilwara",
+    date: "Apr 2017 - Apr 2019",
+    grade: "93.8%",
+    desc: "I successfully completed my Class 10 education at Central Academy Sr. Sec. School, Bhilwara. This institution provided the foundation for my pre-higher education, offering a supportive environment and quality learning experiences that have shaped my academic journey.",
+    degree: "CBSE(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "House of Wears",
+    date: "Jan 2023 - Apr 2023",
+    description:
+      "Our innovative web project revolves around a dynamic online platform for buying and renting clothes. By integrating HTML, CSS, JavaScript, PHP, and MySQL, we've crafted a seamless user experience. Users can effortlessly browse, purchase, and even rent a wide array of clothing items. The website's intuitive interface, backed by robust back-end technologies, ensures smooth transactions and personalized recommendations. It's a comprehensive solution, offering convenience and style for fashion enthusiasts. ",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/how-img.jpg?alt=media&token=f99153ff-fdb6-49ad-9f02-9e724917714d",
+    tags: ["HTML", "CSS", "Javascript", "PHP", "MySQL"],
+    category: "website",
+    github: "https://github.com/jsakshi1109/HouseOFWears",
+  },
+  {
+    id: 1,
+    title: "Sanskriti - Culture of India",
+    date: "Jan 2023 - Apr 2023",
+    description:
+      "This impressive project, built with .NET and C#, introduces a revolutionary online retail experience dedicated to stylish clothing. The platform offers a seamless interface where users can effortlessly browse, select, and purchase fashionable items. Utilizing advanced algorithms, it provides personalized recommendations, ensuring a tailored shopping experience for every user. With secure payment gateways and responsive design, this project transforms conventional online shopping into an intuitive, stylish, and secure venture for fashion enthusiasts.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/Sanskriti-img.jpg?alt=media&token=ab8e8603-934b-4420-8272-dda30f12908c",
+    tags: [
+      ".NET",
+      "C#"
+    ],
+    category: "web app",
+    github: "https://github.com/jsakshi1109/Sanskriti",
+  },
+  {
+    id: 2,
+    title: "Food2Rescue",
+    date: "Jul 2023 - Present",
+    description:
+      "Food2Rescue is an innovative Android application designed to bridge the gap between food service providers and volunteers. By leveraging technology, it efficiently connects surplus meals from restaurants and food businesses with volunteers willing to redistribute these meals to those in need. This initiative not only combats food waste but also addresses the issue of hunger in communities. Food2Rescue stands as a powerful solution, fostering a sustainable approach to food distribution and reducing food wastage significantly.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/food%202%20rescue%20-%20img.jpg?alt=media&token=a0fbb343-6325-4601-ae8e-285ca1e21722",
+    tags: ["Android Studio", "XML", "Kotlin", "Amazon Web Services"],
+    category: "Android app",
+    github: ""
+  },
+  {
+    id: 3,
+    title: "Play Wave",
+    date: "Jul 2023 - Present",
+    description:
+      "This multimedia application, built using XML for user interface design and Java for backend functionality, offers a seamless experience for audio and video playback. The app boasts essential features and controls, ensuring users can enjoy their favorite media content effortlessly. With an intuitive interface crafted in XML and robust functionality powered by Java, the app provides reliable audio and video playback, making it a go-to choice for users seeking a straightforward and efficient multimedia solution.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/personal-portfolio-c7d28.appspot.com/o/play%20wave%20img.jpg?alt=media&token=68594737-ec13-4c55-b32c-65006c7fb4bc",
+    tags: ["Android Studio", "XML", "Java"],
+    category: "Android app",
+    github: "https://github.com/jsakshi1109/EntertainmentApp"
+  }
+];
